340908b8e475cb4ce6f1a659f6a845f5d96be598
Lazcano007 <Pedrolazcano@live.com>
2025-09-15 17:15:35 +0200

    feat: added routes file

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -2,22 +2,24 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './database/db';
+import router from './routes';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-connectDB();
 
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
+
+app.use( cors ({ origin: "http://localhost:5173"})
 );
 
+app.use( express.json());
+
+app.use('/api', router);
+
+connectDB();
 
 app.listen(PORT, async () => {
-  //   await connectDB();
   console.log(`Server is running at http://localhost:${PORT}`);
 });
