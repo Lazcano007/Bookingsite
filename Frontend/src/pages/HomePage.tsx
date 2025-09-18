bb8eab38f06a5d27189a3f8800cf79700559b577
Lazcano007 <Pedrolazcano@live.com>
2025-09-18 15:06:07 +0200

    feat Added minor changes

--- Frontend/src/pages/HomePage.tsx
+++ Frontend/src/pages/HomePage.tsx
@@ -7,15 +7,15 @@ import BookButton from "../components/BookButton";
 
 export default function Service() {
 
-    const services: Service[] = [
-      {id: "1", title: "Kids’ haircut (6–12 years)", price: 400 },
-      {id: "2", title: "Men’s haircut", price: 650 },
-      {id: "3", title: "Beard trim / Wet shave)", price: 400 },
-      {id: "4", title: "Women’s haircut", price: 800 },
-    ];
+  const services: Service[] = [
+    {id: "1", title: "Kids’ haircut (6–12 years)", price: 400 },
+    {id: "2", title: "Men’s haircut", price: 650 },
+    {id: "3", title: "Beard trim / Wet shave)", price: 400 },
+    {id: "4", title: "Women’s haircut", price: 800 },
+  ];
 
     const handleSelect = (service: Service) => {
-        console.log("Selected service:", service);
+      console.log("Selected service:", service);
     };
 
 
