90da9db868cf5418b1c63887646a9f6d0350e8f5
Lazcano007 <Pedrolazcano@live.com>
2025-10-07 20:19:43 +0200

    feat: added a profilePage. Added the design as well for profile and historyPage

--- Frontend/src/pages/ProfilePage.tsx
+++ Frontend/src/pages/ProfilePage.tsx
@@ -9,9 +9,21 @@ export default function Profile() {
 
             <NavbarButtons/>
 
-            <h2 className="profile_selection-title">Profile</h2>
+            <h2 className="profile_selection-title">PROFILE</h2>
 
-            
+            <div className="profile-card">
+                <div className="profile-info">
+                    <div className="form-row">
+                        <label htmlFor="name">Name:</label>
+                        <input id="name" defaultValue={"Pedro"}/>
+                    </div>
+
+                    <div className="form-row">
+                        <label htmlFor="email">Email:</label>
+                        <input id="email" defaultValue={"Lazcano"} />
+                    </div>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
