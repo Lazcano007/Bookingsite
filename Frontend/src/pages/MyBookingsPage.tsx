b2d687990216b5e8ec1eb59febea6b41e32be337
Lazcano007 <Pedrolazcano@live.com>
2025-10-21 12:07:39 +0200

    feat: connected mybooking to backend and rewrote frontend code

--- Frontend/src/pages/MyBookingsPage.tsx
+++ Frontend/src/pages/MyBookingsPage.tsx
@@ -1,18 +1,45 @@
 import NavbarButtons from "../components/NavbarButtons";
 import "../styles/_MyBookings.scss";
+import { api } from "../api/axios";
+import { useEffect, useState } from "react";
 
 type Booking = {
-    id: string;
-    service: string;
-    dateLabel: string;
-    timeLabel: string;
+    _id: string;
+    title: string;
+    date: string;
+    time: string;
+
 }
 
 export default function MyBooking() {
 
-    const booking: Booking[] = [
-        { id: "b1", service: "Men's haircut", dateLabel: "Monday 20 sep", timeLabel: "13.00",},
-    ];
+    const [booking, setBookings] = useState<Booking[]>([]);
+    const [message, setMessage] = useState("");
+
+    useEffect(()=> {
+        async function fetchBookings() {
+            try {
+                const res = await api.get("/bookings/upcoming");
+                setBookings(res.data);
+            } catch (err: any) {
+                setMessage(err.response?.data?.message || "We couldnt fetch your bookings")
+            }
+        }
+        fetchBookings();
+    }, []);
+
+
+    async function handleCancelBooking(id: string) {
+        if (!confirm("Are you sure you want to cancel this appointment")) return;
+
+        try {
+            await api.delete(`/bookings/${id}`);
+            setBookings((prev) => prev.filter((b) => b._id !== id));
+            setMessage("Your booking has been cancelled!")
+        } catch (err: any) {
+            setMessage(err.response?.data?.message || "We couldnt cancel your booking");
+        }
+    }
 
     return (
         <div className="home">
@@ -21,24 +48,25 @@ export default function MyBooking() {
 
                 <h2 className="booking_selection-title">My bookings</h2>
 
+                {message && <p className="booking-message">{message}</p>}
+
+                {booking.length === 0 ? (
+                    <div className="booking-empty"> You have no upcoming bookings!</div>
+                ) : (
                 <div className="booking-list">
                     {booking.map((b)=> (
-                        <div key= {b.id} className="booking-card">
+                        <div key= {b._id} className="booking-card">
                             <div className="booking-row">
                                 <span className="booking-date">
-                                    {b.dateLabel} kl {b.timeLabel}
+                                    {new Date(b.date).toLocaleDateString()} kl {b.time}
                                 </span>
-                                <span className="booking-service">{b.service}</span>
+                                <span className="booking-service">{b.title}</span>
                             </div>
-                            <button className="booking-cancel" onClick={() => { 
-                                console.log("Cancel clicked:", b.id); 
-                                alert("Your cancelled your appointment (Fake)")}}>CANCEL APPOINTMENT</button>
+                            <button className="booking-cancel" onClick={() => handleCancelBooking(b._id)}>CANCEL APPOINTMENT</button>
                         </div>
-                    ))}
-                    {booking.length === 0 && (
-                        <div className="booking-empty"> You have no upcoming bookings.</div>
-                    )}
-                </div>
+                        ))}
+                    </div>
+                )}
             </div>  
         </div>
     );
