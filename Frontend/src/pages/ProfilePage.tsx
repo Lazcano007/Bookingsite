97a95ac61660259d4aa1111b62af3f6db688be76
Lazcano007 <Pedrolazcano@live.com>
2025-10-22 14:23:31 +0200

    feat: deleted empty space in code

--- Frontend/src/pages/ProfilePage.tsx
+++ Frontend/src/pages/ProfilePage.tsx
@@ -2,12 +2,11 @@ import "../styles/_ProfilePage.scss";
 import NavbarButtons from "../components/NavbarButtons";
 
 export default function Profile() {
-
+    
     return (
         <div className="home">
 
             <NavbarButtons/>
-
             <h2 className="profile_selection-title">PROFILE</h2>
 
             <div className="profile-card">
