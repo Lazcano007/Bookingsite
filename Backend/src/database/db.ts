4a79ce7795fbbd6392844757c52029d77c73a427
Lazcano007 <Pedrolazcano@live.com>
2025-11-07 12:39:46 +0100

    feat: deleted all consolelogs

--- Backend/src/database/db.ts
+++ Backend/src/database/db.ts
@@ -7,7 +7,6 @@ async function connectDB() {
   try {
     const URL = process.env.MONGO_URI || '';
     const conn = await mongoose.connect(URL);
-    console.log(`MongoDB connected successfully to: ${conn.connection.host}`);
   } catch (error: unknown) {
     if (error instanceof Error) {
       console.error('Error connecting to MongoDB:', error.message);
